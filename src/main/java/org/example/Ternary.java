d5c166329477823d90abba56ed690e58f42dd716
Tural-Ibrahimov <https://github.com/Tural-Ibrahimov/test-repo.git>
2025-06-15 23:21:20 +0400

    my tasks

---
+++ src/main/java/org/example/Ternary.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+public class Ternary {
+    public static void main(String[] args) {
+        int ay = 2;
+        System.out.println(
+                (ay < 1 || ay > 12) ?
+                        (ay >= 3 && ay <= 5) ? "yaz" :
+                        (ay >= 6 && ay <= 8) ? "yay" :
+                                (ay >= 9 && ay <= 11) ? "payiz" : "qis" : "yalnis ay"
+
+        );
+
+    }
+}
