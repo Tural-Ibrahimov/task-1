a17698afecbcc13a8b05d9f9d6885a54c7f0972b
Tural-Ibrahimov <https://github.com/Tural-Ibrahimov/test-repo.git>
2025-06-16 00:15:12 +0400

    true answer

--- src/main/java/org/example/Ternary.java
+++ src/main/java/org/example/Ternary.java
@@ -3,13 +3,13 @@ package org.example;
 
 public class Ternary {
     public static void main(String[] args) {
-        int ay = 2;
+        int ay = 15;
         System.out.println(
-                (ay < 1 || ay > 12) ?
+                (ay >= 1 && ay <= 12) ?
                         (ay >= 3 && ay <= 5) ? "yaz" :
                         (ay >= 6 && ay <= 8) ? "yay" :
-                                (ay >= 9 && ay <= 11) ? "payiz" : "qis" :
-        (ay >= 12 && ay <= 20) ? "payiz" : "qis"
+                                (ay >= 9 && ay <= 11) ? "payiz" : "qis":"yalnis cavab"
+
 
         );
 
