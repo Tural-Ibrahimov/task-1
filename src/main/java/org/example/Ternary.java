e35e9613dd09fabb53e2c270f5522a33f62757fe
Tural-Ibrahimov <https://github.com/Tural-Ibrahimov/test-repo.git>
2025-06-16 11:57:29 +0400

    true 2 answer

--- src/main/java/org/example/Ternary.java
+++ src/main/java/org/example/Ternary.java
@@ -8,7 +8,7 @@ public class Ternary {
                 (ay >= 1 && ay <= 12) ?
                         (ay >= 3 && ay <= 5) ? "yaz" :
                         (ay >= 6 && ay <= 8) ? "yay" :
-                                (ay >= 9 && ay <= 11) ? "payiz" : "qis":"yalnis cavab"
+                                (ay >= 9 && ay <= 11) ?  "payiz" : "qis":"yalnis cavab"
 
 
         );
